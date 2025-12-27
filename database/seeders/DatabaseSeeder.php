007c5fa37cf52ece2cc17d80bf6e765b357110e5
Tom Löffler <tom.loeffler@linuxtech-it.de>
2025-12-27 14:59:25 +0100

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        $this->call([
+        $this->call([ 
             EinsatzSeeder::class,
         ]);
         $this->call([
