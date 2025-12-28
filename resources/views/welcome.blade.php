a2222320e423c6de37cf6fe2c2800e7a53e62a44
Tom Löffler <tom.loeffler@linuxtech-it.de>
2025-12-28 13:59:52 +0100

    updated picture on home to center

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,16 @@
 .widget{flex:1;min-width:0;display:flex}
 .widget-center{flex:2;display:flex;justify-content:center;align-items:center}
 .widget-center img{width:100%;max-width:100%;height:auto;display:block}
-@media (max-width:640px){.widgets-row{flex-direction:column}.widget-center{width:80%}}
+@media (max-width:640px){.widgets-row{flex-direction:column}.widget-center{width:80%; margin-left: auto; margin-right: auto;}}
 </style>
 
 <div class="widgets-row">
-    <div class="widget widget-left hidden md:block">
+    <div class="widget widget-left hidden">
         <x-aktuelles-widget />
     </div>
 
     <div class="widget widget-center border-4 border-red-700">
-        <img src="{{ Storage::url('images/image.png') }}" alt="Description">
+        <img src="{{ Storage::url('images/image.png') }}" alt="Titelfoto">
     </div>
 
     <div class="widget widget-right hidden md:block">
