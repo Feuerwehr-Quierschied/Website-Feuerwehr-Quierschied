d67163a13dc9066114ef2088d799c0afa745add8
Tom Löffler <tom.loeffler@linuxtech-it.de>
2026-02-11 23:49:10 +0100

    put welcome text on top of widget

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,18 @@
 
     <div class="flex-2 flex flex-col justify-center items-center border-b-4 md:border-4 border-red-700  max-sm:mx-auto">
         <img src="{{ Storage::url('images/image.png') }}" alt="Titelfoto" class="w-full max-w-full h-auto block">
-
     </div>
-    <div class="md:hidden">
+    @if($welcomeIntro ?? null)
+    <div class="md:hidden max-w-4xl w-full mx-auto px-4 mt-4">
+        <div class="relative rounded-xl bg-gray-800 p-6 shadow-lg shadow-red-950/30">
+            <h2 class="text-xl font-bold mb-4 text-gray-900 dark:text-white border-b-2 border-red-700 pb-2">
+                {{ $welcomeIntro->title }}
+            </h2>
+            <p class="text-def-text leading-relaxed whitespace-pre-line">{{ $welcomeIntro->body }}</p>
+        </div>
+    </div>
+    @endif
+    <div class="md:hidden mb-4">
         <x-mobile-widgets-tabbed />
     </div>
 
@@ -18,7 +27,7 @@
     </div>
 </div>
 @if($welcomeIntro ?? null)
-<div class="max-w-4xl mx-auto my-8 px-4 mt-10">
+<div class="hidden md:block max-w-4xl mx-auto my-8 px-4 mt-10">
     <div class="relative rounded-xl bg-gray-800 p-6 shadow-lg shadow-red-950/30">
         <h2 class="text-xl font-bold mb-4 text-gray-900 dark:text-white border-b-2 border-red-700 pb-2">
             {{ $welcomeIntro->title }}
